4ae8db79422131a24e6212adf9c96f9ad962b0c0
Derek Weitzel <djw8605@gmail.com>
2018-09-19 17:16:25 -0500

    Adding postgres

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
+import os
 
 app = Flask(__name__, static_folder='static')
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL') or \
+        'sqlite:////tmp/test.db'
 
 db = SQLAlchemy(app)
 
